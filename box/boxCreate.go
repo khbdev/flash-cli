f0d6f024d2fbd6066c47bd6f41654c1974d60920
Azizbek <webcoderazizbek@gmail.com>
2026-02-10 17:25:18 -0200

    flash

--- box/boxCreate.go
+++ box/boxCreate.go
@@ -32,7 +32,7 @@ func CreateBox(name string) {
 		file.Close()
 		fmt.Println(" Yangi box fayl yaratildi:", boxFilePath)
 	} else {
-		fmt.Println(" Box fayl allaqachon :", boxFilePath)
+		fmt.Println(" Box fayl  :", boxFilePath)
 	}
 
 	cmd := exec.Command("nano", boxFilePath)
